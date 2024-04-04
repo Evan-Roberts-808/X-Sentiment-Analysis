74269e008be4f5b91ec4357e5bfe37014738b343
Evan <eroberts1998@icloud.com>
2024-04-04 18:21:45 -0400

    finished script that prompts user for input, runs sentiment analysis and exports as a csv, doesnt have proper twitter api access levels to read tweets

--- main.py
+++ main.py
@@ -2,8 +2,21 @@ import tweepy
 from textblob import TextBlob
 from dotenv import load_dotenv
 import os
+import csv
 
 load_dotenv()
+
+def get_sentiment_label(sentiment_score):
+    if sentiment_score > 0.7:
+        return 'Strongly Positive'
+    elif sentiment_score > 0.3:
+        return 'Positive'
+    elif sentiment_score > -0.3:
+        return 'Neutral'
+    elif sentiment_score > -0.7:
+        return 'Negative'
+    else:
+        return 'Strongly Negative'
 
 api_key = os.environ.get('API_KEY')
 api_key_secret = os.environ.get('API_KEY_SECRET')
@@ -17,7 +30,16 @@ api = tweepy.API(auth)
 search_query = input('What would you like to search?')
 public_tweets = api.search_tweets(search_query)
 
-for tweet in public_tweets:
-    print(tweet.text)
-    analysis = TextBlob(tweet.text)
-    print(analysis)
+with open(f'{search_query}_sentiment_analysis.csv', 'w', newline='') as csvfile:
+    csv_writer = csv.writer(csvfile)
+    csv_writer.writerow(['Tweet Text', 'Sentiment Label'])
+
+    for tweet in public_tweets:
+        analysis = TextBlob(tweet.text)
+        sentiment_score = analysis.sentiment.polarity
+
+        sentiment_label = get_sentiment_label(sentiment_score)
+
+        csv_writer.writerow([tweet.text, sentiment_label])
+    
+    print(f"Sentiment analysis results for search query: {search_query} were saved to {search_query}_sentiment_analysis.csv")
